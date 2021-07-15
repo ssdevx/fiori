967a8dfe3e7c7bc1f4dce848cdf5985f5e5066e3
Silvestre <ssivasquez@gmail.com>
2021-07-15 22:09:19 +0000

    Hasta la seccion 4

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,15 +1,27 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("ssdev.invoices.controller.MainView", {
-			onInit: function () {
+        return Controller.extend("ssdev.invoices.controller.MainView", {
+            onInit: function () {
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("./model/selectionScreenMenu.json");
+                oView.setModel(oJSONModel, "selectionScreen");
+            },
 
-			}
-		});
-	});
+            onFilter: function (oEvent) {
+
+            },
+
+            onClearFilter: function(oEvent){
+                const oModelSS = this.getView().getModel("selectionScreen");
+                oModelSS.setProperty("/CountryKey", "");
+            }
+        });
+    });
